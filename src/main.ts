fb8dcbb1649adb4ad634f2c00bbd7d9695288e61
cedo-cloud <oderocedric@gmail.com>
2025-05-11 18:26:26 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faTrash)
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
