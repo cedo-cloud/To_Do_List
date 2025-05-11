8a6c07693f4506f22e29acd8e378f1b5deff5640
cedo-cloud <oderocedric@gmail.com>
2025-05-11 18:25:31 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <body>
-    <div class="container">
+    <div class="container" >
+
+      <p><h1><center>To Do List </center></h1></p>
+      <br></br>
+      
       <section>
-        <input type="text" class="input-group" v-model="inputTodo">
+        <input type="text" class="input-group " placeholder="e.g. make a video" v-model="inputTodo">
         <button @click="addTodo()" class="btn-success btn">Add</button>
       </section>
 
       <ul class="list-group" v-for="todo in todoList" key="todo.id"> 
         <li class="list-group-item" v-if="todo!=''">
           {{ todo }}
-          <button @click="() => deleteTodo(todo)" class="btn btn-danger">Delete</button>
+          
+          <button @click="deleteTodo(todo)" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i>
+            <!-- Add the style and icon you want using the Array format -->
+            <font-awesome-icon :icon="['fas', 'trash']" />
+          </button>
         </li>
       </ul>
     </div>
-  </body>
 </template>
 
 <script setup lang="ts">
@@ -33,7 +39,7 @@
       function deleteTodo(todo: string){
         const index = todoList.value.indexOf(todo)
         if(index > -1){
-          todoList.value.splice(1, index)
+          todoList.value.splice(index, 1)
         }
       }
     
@@ -41,6 +47,10 @@
     
 </script>
 
-<style></style>
+<style>
+  
+</style>
+
+    
 
     
